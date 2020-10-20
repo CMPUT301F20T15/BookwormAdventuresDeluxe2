7e75bbe79c817cfd00523bdbb92c769caca91885
naviza <43558584+naviza@users.noreply.github.com>
2020-10-19 22:25:14 -0600

    Add ISBN Scan

--- app/src/main/java/com/example/bookwormadventuresdeluxe2/MyBooksFragment.java
+++ app/src/main/java/com/example/bookwormadventuresdeluxe2/MyBooksFragment.java
@@ -1,18 +1,20 @@
 package com.example.bookwormadventuresdeluxe2;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 /**
  * A {@link Fragment} subclass for navbar menu item 1.
  */
 public class MyBooksFragment extends Fragment
 {
+    public int LAUNCH_SCAN_ISBN = 1; // a code to check for result return
+    public int LAUNCH_SCAN_ISBN_RETURN = 2; // a code to check for result return
 
     public MyBooksFragment()
     {
@@ -26,4 +28,16 @@ public class MyBooksFragment extends Fragment
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_books, container, false);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == LAUNCH_SCAN_ISBN)
+        {
+//            String testResult = data.getStringExtra(EXTRA_KEY_TEST);
+            // TODO: Do something with your extra data
+        }
+    }
+
 }
