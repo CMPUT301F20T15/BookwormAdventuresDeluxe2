1e07e59ae7e414d02b1b2b417cb06a5c3fad0045
Hudson <huddy987@gmail.com>
2020-10-31 22:00:39 -0600

    Remove unused onClick handler
    It was causing TravisCI to fail

--- app/src/main/java/com/example/bookwormadventuresdeluxe2/CreateAccountActivity.java
+++ app/src/main/java/com/example/bookwormadventuresdeluxe2/CreateAccountActivity.java
@@ -17,17 +17,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bookwormadventuresdeluxe2.Utilities.EditTextValidator;
 import com.example.bookwormadventuresdeluxe2.Utilities.UserCredentialAPI;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
-
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
