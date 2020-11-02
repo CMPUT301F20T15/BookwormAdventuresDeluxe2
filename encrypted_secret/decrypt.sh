7fd7e5d8d4cb53a72987ac70e164fd3c0e2ae6df
Hudson <huddy987@gmail.com>
2020-11-01 20:10:51 -0700

    test ls

--- encrypted_secret/decrypt.sh
+++ encrypted_secret/decrypt.sh
@@ -3,6 +3,9 @@
 # Decrypt the file
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
+ls
+cd $HOME/runner/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/app/src/main/java/res/values/
+ls
 gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPTION_KEY_PASSWORD" \
---output $HOME/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/app/src/main/java/res/values/secrets.xml \
-$HOME/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/encrypted_secret/secrets.xml.gpg
+--output $HOME/runner/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/encrypted_secret/secrets.xml.gpg \
+$HOME/runner/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/app/src/main/java/res/values/secrets.xml
