f05375579996131c475d30db518e4d323da4638f
Hudson <huddy987@gmail.com>
2020-11-01 20:16:01 -0700

    another try

--- encrypted_secret/decrypt.sh
+++ encrypted_secret/decrypt.sh
@@ -4,5 +4,5 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPTION_KEY_PASSWORD" \
---output $HOME/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/encrypted_secret/secrets.xml.gpg \
-./app/src/main/java/res/values/secrets.xml
+--output ./app/src/main/java/res/values/secrets.xml \
+$HOME/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/encrypted_secret/secrets.xml.gpg
