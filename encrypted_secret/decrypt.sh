917f31538f55949b65c7e6b39204a5ee06f8f419
Hudson <huddy987@gmail.com>
2020-11-01 19:54:32 -0700

    Add encryption key environment variable

--- encrypted_secret/decrypt.sh
+++ encrypted_secret/decrypt.sh
@@ -4,4 +4,5 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPTION_KEY_PASSWORD" \
---output $HOME/runner/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/encrypted_secret/secrets.xml.gpg $HOME/runner/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/app/src/main/java/res/values/secrets.xml
+--output $HOME/runner/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/encrypted_secret/secrets.xml.gpg \
+$HOME/runner/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/app/src/main/java/res/values/secrets.xml
