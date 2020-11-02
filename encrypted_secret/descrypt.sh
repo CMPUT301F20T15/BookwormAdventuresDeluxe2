f96b42a4210114c0faf1452101761193e66e2168
Hudson <huddy987@gmail.com>
2020-11-01 19:42:00 -0700

    Add decryption script

---
+++ encrypted_secret/descrypt.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Decrypt the file
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPTION_KEY_PASSWORD" \
+--output $HOME/runner/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/encrypted_secret/secrets.xml.gpg $HOME/runner/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/app/src/main/java/res/values/secrets.xml
