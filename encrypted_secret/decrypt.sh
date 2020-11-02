f37c946faa75f9ff4a2a3038613ca33ed9691179
Hudson <huddy987@gmail.com>
2020-11-01 20:21:39 -0700

    fix path?

--- encrypted_secret/decrypt.sh
+++ encrypted_secret/decrypt.sh
@@ -3,19 +3,6 @@
 # Decrypt the file
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-ls
-cd app
-ls
-cd src
-ls
-cd main
-ls
-cd java
-ls
-cd res
-ls
-cd values
-ls
 gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPTION_KEY_PASSWORD" \
---output ./app/src/main/java/res/values/secrets.xml \
+--output $HOME/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/app/src/main/res/values/secrets.xml \
 $HOME/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/encrypted_secret/secrets.xml.gpg
