08fd50725a63537b40de685d999bfd9308414423
naviza <43558584+naviza@users.noreply.github.com>
2020-11-06 01:59:18 -0700

    Cleaned up tests

--- app/src/test/java/com/example/bookwormadventuresdeluxe2/BookAugmentedStatusTest.java
+++ app/src/test/java/com/example/bookwormadventuresdeluxe2/BookAugmentedStatusTest.java
@@ -16,6 +16,8 @@ import static org.junit.Assert.assertEquals;
 public class BookAugmentedStatusTest
 {
     private String testBorrower;
+    private String anotherUser;
+    private String owner;
     private Book book;
 
     /**
@@ -25,6 +27,8 @@ public class BookAugmentedStatusTest
     public void setup()
     {
         testBorrower = "testBorrower";
+        anotherUser = "anotherUser";
+        owner = "owner";
         book = makeBook();
     }
 
@@ -35,7 +39,7 @@ public class BookAugmentedStatusTest
      */
     private Book makeBook()
     {
-        return new Book("owner", "title", "author",
+        return new Book(owner, "title", "author",
                 "desc", "1234567890",
                 Status.Available, "");
     }
@@ -58,7 +62,12 @@ public class BookAugmentedStatusTest
     {
         book.addRequester(testBorrower);
         book.setStatus(Status.Available);
+        // The test borrowed should see that it is requested
         assertEquals(book.getAugmentStatus(testBorrower), Status.Requested);
+        // The owner should also see the book as requested
+        assertEquals(book.getAugmentStatus(owner), Status.Requested);
+        // A different user should be able to see it as available
+        assertEquals(book.getAugmentStatus(anotherUser), Status.Available);
     }
 
     /**
@@ -69,35 +78,27 @@ public class BookAugmentedStatusTest
     {
         book.addRequester(testBorrower);
         book.setStatus(Status.Accepted);
+        // The book should be accepted for borrower and owner
         assertEquals(book.getAugmentStatus(testBorrower), Status.Accepted);
+        assertEquals(book.getAugmentStatus(owner), Status.Accepted);
     }
 
     /**
      * The book is being passed from owner to borrower
-     * The owner is viewing the book
      */
     @Test
-    public void testGetAugmentStatusbPendingOwner()
+    public void testGetAugmentStatusBPending()
     {
         book.addRequester(testBorrower);
         book.setStatus(Status.bPending);
-        assertEquals(book.getAugmentStatus("owner"), Status.Borrowed);
-    }
-
-    /**
-     * The book is being passed from owner to borrower
-     * The borrower is viewing the book
-     */
-    @Test
-    public void testGetAugmentStatusbPendingBorrower()
-    {
-        book.addRequester(testBorrower);
-        book.setStatus(Status.bPending);
+        // From the owner's perspective, the borrower already has it
+        assertEquals(book.getAugmentStatus(owner), Status.Borrowed);
+        // From the borrower's perspective, the book request is just accepted
         assertEquals(book.getAugmentStatus(testBorrower), Status.Accepted);
     }
 
     /**
-     * The book exchange has occured
+     * The book exchange has occurred
      * The borrower now has the book
      */
     @Test
@@ -105,32 +106,24 @@ public class BookAugmentedStatusTest
     {
         book.addRequester(testBorrower);
         book.setStatus(Status.Borrowed);
+        // The book should be borrowed for borrower and owner
         assertEquals(book.getAugmentStatus(testBorrower), Status.Borrowed);
+        assertEquals(book.getAugmentStatus(owner), Status.Borrowed);
     }
 
     /**
      * The book is being passed from borrower back to owner
-     * The borrower is viewing the book
      */
     @Test
-    public void testGetAugmentStatusRPendingBorrower()
+    public void testGetAugmentStatusRPending()
     {
         book.addRequester(testBorrower);
         book.setStatus(Status.rPending);
+        // From the borrower's perspective, they've returned the book
         assertEquals(book.getAugmentStatus(testBorrower), Status.Available);
+        // From the owner's perspective, the borrower still has the book
+        assertEquals(book.getAugmentStatus(owner), Status.Borrowed);
+        // potential TODO: Only make the book Available for the borrower
+        // assertEquals(book.getAugmentStatus(anotherUser), Status.Borrowed);
     }
-
-    /**
-     * The book is being passed from borrower back to owner
-     * The owner is viewing the book
-     */
-    @Test
-    public void testGetAugmentStatusRPendingOwner()
-    {
-        book.addRequester(testBorrower);
-        book.setStatus(Status.rPending);
-        assertEquals(book.getAugmentStatus("owner"), Status.Borrowed);
-    }
-
-
 }
