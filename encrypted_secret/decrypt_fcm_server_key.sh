91a4c7760d62d1905735983159d7abb6d3ecfe86
Jawad Hossain <jhossain@ualberta.ca>
2020-11-26 20:29:58 -0700

    Organize File Tree

--- encrypted_secret/decrypt_fcm_server_key.sh
+++ encrypted_secret/decrypt_fcm_server_key.sh
@@ -4,5 +4,5 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$FCM_PASSPHRASE" \
---output $HOME/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/app/src/main/java/com/example/bookwormadventuresdeluxe2/NotificationUtility/CloudMessagingServerKeyConstant.java \
+--output $HOME/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/app/src/main/java/com/example/bookwormadventuresdeluxe2/Utilities/NotificationUtility/CloudMessagingServerKeyConstant.java \
 $HOME/work/BookwormAdventuresDeluxe2/BookwormAdventuresDeluxe2/encrypted_secret/CloudMessagingServerKeyConstant.java.gpg
